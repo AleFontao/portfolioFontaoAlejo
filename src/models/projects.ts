dc4072f1b640913680601c2fea7f31e64646e221
Alejo Fontao <alefonta@hotmail.com.ar>
2024-03-09 15:02:38 -0300

    responsive

--- src/models/projects.ts
+++ src/models/projects.ts
@@ -5,6 +5,8 @@ export interface Project {
     url?: string;
     github?: string;
     position: string;
+    area: string;
+    tecnologies: string[];
 }
 
 export const Projects: Project[] = [
@@ -15,7 +17,9 @@ export const Projects: Project[] = [
             "./assets/wofory.png",
         descriptionProject: "El proyecto es una página web que sirve como intermediario entre trabajadores del hogar (como plomeros, electricistas, etc.) y usuarios que necesitan soluciones rápidas para sus hogares. La plataforma cuenta con funciones como registro de usuarios, un calendario para gestionar citas, búsqueda por proximidad, chat en tiempo real, entre otras características.",
         url: "https://www.wofory.com",
-        position: ""
+        position: "",
+        area: "FrontEnd - BackEnd",
+        tecnologies: ["Angular", "ASP.NET", "SQL Server"]
     },
     {
         titleProject:
@@ -24,7 +28,9 @@ export const Projects: Project[] = [
             "./assets/indexador.png",
         descriptionProject: "El proyecto consiste en la indexación de una cantidad determinada de archivos de texto, para luego permitir la búsqueda de palabras o conjuntos de palabras específicas dentro de dichos archivos. Este proceso se lleva a cabo de manera similar a un buscador convencional que utilizamos a diario. El proyecto ha sido desarrollado utilizando JAVA/SpringBoot en la parte del servidor, y Bootstrap en la interfaz de usuario (FrontEnd). La aplicación es fácil de usar y se basa en una tecnología robusta que garantiza una búsqueda rápida y precisa en los archivos indexados.",
         github: "https://github.com/AleFontao/BUSCADOR-DLC",
-        position: ""
+        position: "",
+        area: "FrontEnd",
+        tecnologies: ["Angular"]
     },
     {
         titleProject:
@@ -34,7 +40,9 @@ export const Projects: Project[] = [
         descriptionProject: "El proyecto es una interfaz visual similar a la de Netflix, desarrollada en Angular. Los usuarios pueden navegar por diferentes categorías de contenido y ver detalles de cada elemento. Es altamente interactiva y responsiva, proporcionando una experiencia de usuario fluida y envolvente.",
         url: "https://alefontao.github.io/NetflixClone/",
         github: "https://github.com/AleFontao/NetflixClone",
-        position: ""
+        position: "",
+        area: "FrontEnd",
+        tecnologies: ["Angular"]
     },
     {
         titleProject:
@@ -43,7 +51,9 @@ export const Projects: Project[] = [
             "./assets/iPAdressTracker.png",
         descriptionProject: "El proyecto es una interfaz visual desarrollada con Angular que te permite localizar en un mapa la ubicación asociada a una dirección IP pública. Esta aplicación es útil para obtener información geográfica sobre una dirección IP, lo que puede ser útil en el análisis de datos o en la identificación de problemas de seguridad. La interfaz es sencilla e intuitiva",
         url: "https://alefontao.github.io/IPAddressTracker/",
-        position: ""
+        position: "",
+        area: "FrontEnd",
+        tecnologies: ["Angular"]
     },
 ]
 
