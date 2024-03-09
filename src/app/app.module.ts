5c0ae062517a9c9cb3532b2ca71320a289b26ee8
Alejo Fontao <alefonta@hotmail.com.ar>
2024-03-09 10:53:11 -0300

    title and buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from 'src/components/navBar/navBar.component';
 import { LandingPageComponent } from 'src/components/landing-page/landing-page.component';
+import { PresentationComponent } from 'src/components/presentation/presentation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    PresentationComponent
   ],
   imports: [
     BrowserModule,
